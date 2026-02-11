fe998d0ef82cbdeceb4371767bacea113ff4cb99
Vinicius Araujo <vinidev.morais@gmail.com>
2026-02-11 17:08:43 -0300

    feat: created base entity

---
+++ DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace DevFreela.Core;
+
+public abstract class BaseEntity
+{
+    public int Id { get; private set; }
+    
+    protected BaseEntity() 
+}
---
+++ DevFreela.Core/Entities/Skill.cs
@@ -0,0 +1,7 @@
+namespace DevFreela.Core;
+
+public class Skill
+{
+    public int Description { get; private set; }
+    public DateTime CreatedAt { get; set; }
+}
